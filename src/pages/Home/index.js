702009d5f4400e2ddd7528e3277d7b537dd09476
Fabrício Dias <dias.fabricio0812@gmail.com>
2025-04-17 14:17:38 -0300

    Criando a Home e fazendo a requisição HTTP

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,27 @@
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+
+//URL da API /movie/now_playing?api_key=fd5bb88fd74221037110d433a74a35ed&language=pt-BR
+
 export default function Home() {
+  const [filmes, setFilmes] = useState([]);
+
+  useEffect(() => {
+    async function loadFilmes() {
+      const response = await api.get("movie/now_playing", {
+        params: {
+          api_key: "fd5bb88fd74221037110d433a74a35ed",
+          language: "pt-BR",
+          page: 1,
+        },
+      });
+
+      console.log(response.data.results);
+    }
+
+    loadFilmes();
+  }, []);
+
   return (
     <div>
       <h1>Bem vindo a home!</h1>
