4c492e6b4c85bafda96d037f81e1fd80427941b6
Fabrício Dias <dias.fabricio0812@gmail.com>
2025-04-17 15:27:07 -0300

    Pagina favoritos

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 
 import Home from "./pages/Home";
 import Filme from "./pages/Filme";
+import Favoritos from "./pages/Favoritos";
 
 import Erro from "./pages/Erro";
 
@@ -14,6 +15,7 @@ export default function RoutesApp() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/filme/:id" element={<Filme />} />
+        <Route path="/favoritos" element={<Favoritos />} />
 
         <Route path="*" element={<Erro />} />
       </Routes>
